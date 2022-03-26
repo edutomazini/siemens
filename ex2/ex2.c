bb130f752a788f35ac133cc9579c7a05604af2e5
edutomazini <eduardo_tomazini@msn.com>
2022-03-25 21:47:17 -0300

    in 1 line

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -4,13 +4,5 @@ int main(void)
 {
     int number = 100;
     for(int line = 1; line <= number; line++)
-    {
-        //if (linha % 15 == 0) printf("FooBaa %i\n", linha);
-        //else if (linha % 3 == 0) printf("Foo %i\n", linha);
-        //else if (linha % 5 == 0) printf("Baa %i\n", linha);
-        //else printf("%i\n", linha);
-
         (line % 15 == 0) ? printf("FooBaa\n") : (line % 3 == 0) ? printf("Foo\n") : (line % 5 == 0) ? printf("Baa\n") : printf("%i\n", line);
-    }
-
 }
